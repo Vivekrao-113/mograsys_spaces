cb6a184d2fac9fbb4bf90973cd40817249ea75fa
Vivekrao-113 <vivekrao113@gmail.com>
2022-10-21 11:40:24 +0530

    Initial Commit

---
+++ Shared/mograsys_spacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  mograsys_spacesApp.swift
+//  Shared
+//
+//  Created by Shahaji on 21/10/22.
+//
+
+import SwiftUI
+
+@main
+struct mograsys_spacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
